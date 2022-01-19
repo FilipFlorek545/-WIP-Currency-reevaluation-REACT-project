cc02342fd169c6cb6abfebf796442682229f6fb3
Filip Florek <florson467@icloud.com>
2022-01-19 13:32:47 +0100

    Added logo

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Exchanger from '../helpers/Exchanger';
 import Header from './/Header'
 import "./App.css"
+import Logo from './/forms/logo.png'
 function App() {
   return (
     <div className="App">
+      <img src={Logo}/>
       <Header className='header'>Przelicz wybraną walutę na PLN!</Header>
       <Exchanger />
     </div>
